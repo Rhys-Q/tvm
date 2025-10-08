5fb1ea33f606581406c354f53099a641226bf147
qinzhiqi <qinzhiqi@dg.com>
2025-10-09 00:02:16 +0800

    save

--- src/relax/transform/rewrite_cuda_graph.cc
+++ src/relax/transform/rewrite_cuda_graph.cc
@@ -253,11 +253,11 @@ class CUDAGraphRewritePlanner : public ExprVisitor {
           Array<tir::Var> symbolic_vars = DefinableTIRVarsInStructInfo(
               Downcast<StructInfo>(func->params[i]->struct_info_.value()));
           if (i < num_inputs.IntValue()) {
-            // hack, fix it later
-            if (i !=0 && i!=7){
-              static_vars_.insert(func->params[i].get());
+            // // hack, fix it later
+            // if (i !=0 && i!=7){
+            static_vars_.insert(func->params[i].get());
 
-            }
+            // }
             for (const auto& symbolic_var : symbolic_vars) {
               if (capture_symbolic_var_name_hints.count(symbolic_var->name_hint)) {
                 capture_symbolic_vars_.insert(symbolic_var.get());
